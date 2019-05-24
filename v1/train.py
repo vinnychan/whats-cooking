bca038519cec8e87eaac8f0911bfb7760e1aebf1
Frank Rui <frank.rui@sap.com>
2019-05-24 11:31:57 -0700

    Add evaluation accuracy function

--- v1/train.py
+++ v1/train.py
@@ -16,7 +16,7 @@ n_hidden = 128
 n_epochs = 100000
 print_every = 5000
 plot_every = 1000
-learning_rate = 0.005 # If you set this too high, it might explode. If too low, it might not learn
+learning_rate = 0.001 # If you set this too high, it might explode. If too low, it might not learn
 
 pickle_in = open("glove.pickle", "rb")
 glove_dict = pickle.load(pickle_in)
@@ -123,3 +123,28 @@ for epoch in range(1, n_epochs + 1):
 
 torch.save(rnn, 'char-rnn-classification.pt')
 
+# Keep track of correct guesses in a confusion matrix
+confusion = torch.zeros(n_categories, n_categories)
+n_confusion = 10000
+count_correct = 0
+
+def evaluate(line_tensor):
+    hidden = rnn.initHidden()
+
+    for i in range(line_tensor.size()[0]):
+        output, hidden = rnn(line_tensor[i], hidden)
+
+    return output
+
+# Go through a bunch of examples and record which are correctly guessed
+for i in range(n_confusion):
+    category, line, category_tensor, line_tensor = randomTrainingPair()
+    output = evaluate(line_tensor)
+    guess, guess_i = categoryFromOutput(output)
+    category_i = all_categories.index(category)
+    confusion[category_i][guess_i] += 1
+    if category_i == guess_i:
+        count_correct += 1
+
+print('accuracy:')
+print(count_correct / n_confusion)
